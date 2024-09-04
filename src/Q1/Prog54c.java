ac3bfca786a17ff30437327dd60e131626ab1325
Kayden Weatherford <weatherfor.k@stu.janesville.k12.wi.us>
2024-09-04 11:37:32 -0500

    We Ball with 54c

---
+++ src/Q1/Prog54c.java
@@ -0,0 +1,26 @@
+package Q1;
+import java.util.*;
+import java.lang.Math;
+
+public class Prog54c {
+    public static void main(String[] args){
+        double r, pi = 3.14159;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Enter the Radius: ");
+        r = inp.nextDouble();
+
+        double circ = (2 * pi) * r;
+        System.out.println("The circumference of the circle is: " + circ);
+
+        double area = pi * Math.pow(r, 2);
+        System.out.println("The area of the circle is: " + area);
+
+        /*
+        Enter the Radius: 3.712
+        The circumference of the circle is: 23.32316416
+        The area of the circle is: 43.28779268096
+         */
+
+    }
+}
