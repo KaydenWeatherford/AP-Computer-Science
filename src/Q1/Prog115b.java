e29ef5eda911d6d98890d4a15d1f0068f7cd3ce8
Kayden Weatherford <weatherfor.k@stu.janesville.k12.wi.us>
2024-09-23 11:31:32 -0500

    115b & 122a

---
+++ src/Q1/Prog115b.java
@@ -0,0 +1,33 @@
+package Q1;
+
+public class Prog115b {
+    public static void main(String[] args) {
+        int num = 0;
+
+        while (num != 36){
+            num += 2;
+            System.out.println(num);
+        }
+    }
+}
+/*
+2
+4
+6
+8
+10
+12
+14
+16
+18
+20
+22
+24
+26
+28
+30
+32
+34
+36
+
+ */
